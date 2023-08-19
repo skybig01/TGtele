4bd3981ada6a611aab8b9b41447d90d5cbd37446
ahadigitalpay <126970567+ahadigitalpay@users.noreply.github.com>
2023-08-19 19:51:53 +0800

    first commit

---
+++ credentials.py
@@ -0,0 +1,8 @@
+# enter your API credentials below - generate at https://my.telegram.org/auth
+api_id = 24672347
+api_hash = "b0b3a558f0767c1bea4deea7824f831a"
+
+#THIS IS JUST A GENERIC API_ID AND API_HASH!
+
+
+
